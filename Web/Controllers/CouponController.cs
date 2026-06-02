0fe46c09612eea6df693f4ff03f60b628b9dc6d7
Hector Perez <rivasj8899@gmail.com>
2026-06-01 22:34:50 -0600

    Adding the CreateCoupon form

--- Web/Controllers/CouponController.cs
+++ Web/Controllers/CouponController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using Web.Models;
 using Web.Service.IService;
 
@@ -23,6 +24,30 @@ namespace Web.Controllers
                 list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
             }
             return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(model);
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = response.Message ?? "Coupon create Successfully! ";
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+                else
+                {
+                    TempData["error"] = response?.Message ?? "Error creating coupon";
+                }
+            }
+            return View(model);
         }
     }
 }
